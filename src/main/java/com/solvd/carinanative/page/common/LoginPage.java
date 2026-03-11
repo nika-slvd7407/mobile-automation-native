624a989156ec4656cb70d0aa53937b4833c53546
nika7407 <ntsalkalamanidze@solvd.com>
2026-03-11 17:35:58 +0400

    #task-1 added login tests

--- src/main/java/com/solvd/carinanative/page/common/LoginPage.java
+++ src/main/java/com/solvd/carinanative/page/common/LoginPage.java
@@ -2,19 +2,23 @@ package com.solvd.carinanative.page.common;
 
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 public abstract class LoginPage extends AbstractPage {
 
     @FindBy(xpath = "//android.widget.EditText[contains (@content-desc, 'test-Password')]")
-    ExtendedWebElement passwordInput;
+    private ExtendedWebElement passwordInput;
 
     @FindBy(xpath = "//android.widget.EditText[contains (@content-desc, 'test-Username')]")
-    ExtendedWebElement userNameInput;
+    private ExtendedWebElement userNameInput;
 
     @FindBy(xpath = "//android.view.ViewGroup[contains (@content-desc, 'test-LOGIN')]")
-    ExtendedWebElement loginButton;
+    private ExtendedWebElement loginButton;
+
+    private By errorMessage = By.xpath("//android.view.ViewGroup[contains (@content-desc, 'test-Error message')]//android.widget.TextView");
+    private By lockedOutErrorMessage = By.xpath("//android.view.ViewGroup[contains (@content-desc, 'test-Error message')//android.widget.TextView[contains (@text, 'locked out')]");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -26,6 +30,14 @@ public abstract class LoginPage extends AbstractPage {
             passwordInput.type(password);
             loginButton.click();
             return initPage(getDriver(), ProductsPage.class);
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return findExtendedWebElement(errorMessage).isDisplayed();
+    }
+
+    public String getErrorMessageText() {
+        return findExtendedWebElement(errorMessage).getAttribute("text");
     }
 
 }
--- src/test/java/com/solvd/tests/LoginTest.java
+++ src/test/java/com/solvd/tests/LoginTest.java
@@ -4,18 +4,45 @@ import com.solvd.carinanative.page.common.BasePage;
 import com.solvd.carinanative.page.common.LoginPage;
 import com.solvd.carinanative.page.common.ProductsPage;
 import com.zebrunner.carina.core.AbstractTest;
+import com.zebrunner.carina.utils.R;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.sound.midi.SysexMessage;
+
 public class LoginTest extends AbstractTest {
 
+    private static final String CORRECT_USERNAME = R.TESTDATA.get("CORRECT_USERNAME");
+    private static final String CORRECT_PASSWORD = R.TESTDATA.get("CORRECT_PASSWORD");
+    private static final String INCORRECT_USERNAME = R.TESTDATA.get("INCORRECT_USERNAME");
+    private static final String INCORRECT_PASSWORD = R.TESTDATA.get("INCORRECT_PASSWORD");
+    private static final String LOCKED_OUT_USERNAME = R.TESTDATA.get("LOCKED_OUT_USERNAME");
+
     @Test(description = "Test for correct login functionality")
-    public void testLogin() {
+    public void verifyLoginWithCorrectCredentials() {
         BasePage basePage = basePage = initPage(getDriver(), BasePage.class);
         LoginPage loginPage = basePage.openApp();
-        ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
+        ProductsPage productsPage = loginPage.login(CORRECT_USERNAME, CORRECT_PASSWORD);
 
         Assert.assertTrue(productsPage.isPageOpened(), "Products page is not opened after login");
     }
 
+    @Test(description = "Test for incorrect login functionality")
+    public void verifyInabilityToLoginWithIncorrectCredentials(){
+        BasePage basePage = basePage = initPage(getDriver(), BasePage.class);
+        LoginPage loginPage = basePage.openApp();
+        loginPage.login(INCORRECT_USERNAME, INCORRECT_PASSWORD);
+
+        Assert.assertTrue(loginPage.isErrorMessageDisplayed(), "Error message is not shown for incorrect login");
+    }
+
+    @Test(description = "Test for incorrect login functionality")
+    public void verifyInabilityToLoginWithUsersLockedOutCredentials(){
+        BasePage basePage = basePage = initPage(getDriver(), BasePage.class);
+        LoginPage loginPage = basePage.openApp();
+        loginPage.login(LOCKED_OUT_USERNAME, CORRECT_PASSWORD);
+        String errorMessageText = loginPage.getErrorMessageText();
+
+        Assert.assertTrue(errorMessageText.contains("locked out"), "Error message is not shown for incorrect login");
+    }
 }
